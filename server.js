dca36922fa4d0bf915a83203b3f8c0a51b3b52fe
Jose Annunziato <jannunzi@gmail.com>
2015-08-20 23:52:51 -0400

    Added body-parser to parse user from body, print to console

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 
+// for parsing application/json
+app.use(bodyParser.json());
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.post("/rest/user", function(req, res)
 {
-  console.log("POST /rest/user");
+  var user = req.body
+  console.log(user);
 });
 
 app.listen(3000);
