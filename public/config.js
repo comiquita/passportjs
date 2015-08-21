278fcf6e614f96b4c82d6c148f6c6fff69a908a5
Jose Annunziato <jannunzi@gmail.com>
2015-08-20 23:06:19 -0400

    Added routing configuration for placeholder pages

---
+++ public/config.js
@@ -0,0 +1,17 @@
+(function()
+{
+  angular
+    .module("WhiteBoardApp")
+    .config(Config);
+  
+  function Config($routeProvider)
+  {
+    $routeProvider
+      .when("/home",     {templateUrl: "home/home.view.html"})    
+      .when("/register", {templateUrl: "register/register.view.html"})    
+      .when("/login",    {templateUrl: "login/login.view.html"})    
+      .when("/profile",  {templateUrl: "profile/profile.view.html"})    
+      .when("/admin",    {templateUrl: "admin/admin.view.html"})
+      .otherwise({redirectTo: "/home"});    
+  }
+})();
