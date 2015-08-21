7f4a0508d4711cb0dd27ae03827ef4279260b21f
Jose Annunziato <jannunzi@gmail.com>
2015-08-21 12:55:22 -0400

    Moved user rest api to separate file

--- server.js
+++ server.js
@@ -5,53 +5,9 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/wbdb');
 
 app.use(express.static(__dirname + '/public'));
-
-// for parsing application/json
 app.use(bodyParser.json());
-// for parsing application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var User = require("./user/user.model.js")();
-
-app.post("/rest/user", function(req, res)
-{
-  var user = req.body
-  user.roles = ["student"];
-  User.findOne({username: user.username}, function(err, existingUser)
-  {
-    if(existingUser != null)
-    {
-      res.json(null);
-      return;
-    }
-    else
-    {
-      User.create(user, function(err, result)
-      {
-        res.json(result);
-      });
-    }
-  });
-});
-
-app.post("/rest/login", function(req, res)
-{
-  var user = req.body;
-  User.findOne({username: user.username, password: user.password}, function(err, foundUser)
-  {
-    res.json(foundUser);
-  });
-});
-
-app.put("/rest/update", function(req, res)
-{
-  User.findById(req.body._id, function(err, foundUser)
-  {
-    foundUser.update(req.body, function(err, count)
-    {
-      res.send(count);
-    });
-  });
-});
+var UserService = require("./user/user.service.js")(app);
 
 app.listen(3000);
---
+++ user/user.service.js
@@ -0,0 +1,45 @@
+var User = require("./user.model.js")();
+
+module.exports = function(app)
+{
+  app.post("/rest/user", function(req, res)
+  {
+    var user = req.body
+    user.roles = ["student"];
+    User.findOne({username: user.username}, function(err, existingUser)
+    {
+      if(existingUser != null)
+      {
+        res.json(null);
+        return;
+      }
+      else
+      {
+        User.create(user, function(err, result)
+        {
+          res.json(result);
+        });
+      }
+    });
+  });
+  
+  app.post("/rest/login", function(req, res)
+  {
+    var user = req.body;
+    User.findOne({username: user.username, password: user.password}, function(err, foundUser)
+    {
+      res.json(foundUser);
+    });
+  });
+  
+  app.put("/rest/update", function(req, res)
+  {
+    User.findById(req.body._id, function(err, foundUser)
+    {
+      foundUser.update(req.body, function(err, count)
+      {
+        res.send(count);
+      });
+    });
+  });
+}
