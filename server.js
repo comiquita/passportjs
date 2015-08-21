4bbff264bdd0aa963508520da3f98fe8aa09ae51
Jose Annunziato <jannunzi@gmail.com>
2015-08-21 13:48:22 -0400

    Required and configured passport, passport-local, cookie-parser, express-session

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var passport      = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/wbdb');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'this is the secret' }));
+app.use(cookieParser())
+app.use(passport.initialize());
+app.use(passport.session());
 
 var UserService = require("./user/user.service.js")(app);
 
