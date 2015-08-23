c1b546fbb515725d3afd8f81ca9fcdcf404b9607
Jose Annunziato <jannunzi@gmail.com>
2015-08-23 15:07:54 -0400

    Implement Add New User event handler in the admin controller

--- public/admin/admin.controller.js
+++ public/admin/admin.controller.js
@@ -7,10 +7,26 @@
   function AdminController(UserService)
   {
     var vm = this;
-    
-    UserService.findAllUsers(function(response)
+    vm.addUser = addUser;
+
+    function addUser(newUser)
     {
-      vm.users = response;
-    });
+      UserService.createUser(newUser, function(response)
+      {
+        findAllUsers(setAllUsers);
+      });
+    }
+
+    function findAllUsers(callback)
+    {
+      UserService.findAllUsers(callback);
+    }
+
+    function setAllUsers(users)
+    {
+      vm.users = users;
+    }
+
+    findAllUsers(setAllUsers);
   }
 })();
